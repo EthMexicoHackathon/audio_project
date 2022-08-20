8a4946391283acf09424a52ba8840c49e9ad88d3
Robertas <rcekanauskasnl@gmail.com>
2022-08-20 15:33:16 -0500

    apollo + explore audio publicatiosn

--- website/src/App.js
+++ website/src/App.js
@@ -9,6 +9,43 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignMessageButton from "./components/SignMessageButton";
+import ExplorePublicationsTest from "./pages/ExplorePublicationsTest";
+
+// this is showing you how you use it with react for example
+// if your using node or something else you can import using
+// @apollo/client/core!
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+  ApolloProvider,
+} from "@apollo/client";
+
+const httpLink = new HttpLink({ uri: "https://api-mumbai.lens.dev/" });
+
+// example how you can pass in the x-access-token into requests using `ApolloLink`
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  // if your using node etc you have to handle your auth different
+  const token = localStorage.getItem("auth_token");
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      "x-access-token": token ? `Bearer ${token}` : "",
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const alchemyId = "WxH_mAU0XciJz4PAFStdOYCvYTnXwTdz";
 
 if (!window.Buffer) window.Buffer = Buffer;
@@ -24,15 +61,18 @@ const client = createClient(
 const App = () => {
   return (
     <WagmiConfig client={client}>
-      <ConnectKitProvider>
-        <BrowserRouter>
-          <Navbar />
-          <SignMessageButton />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </ConnectKitProvider>
+      <ApolloProvider client={apolloClient}>
+        <ConnectKitProvider>
+          <BrowserRouter>
+            <Navbar />
+            <SignMessageButton />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/explore" element={<ExplorePublicationsTest />} />
+            </Routes>
+          </BrowserRouter>
+        </ConnectKitProvider>
+      </ApolloProvider>
     </WagmiConfig>
   );
 };
---
+++ website/src/pages/ExplorePublicationsTest.jsx
@@ -0,0 +1,21 @@
+import { useQuery, gql } from "@apollo/client";
+import { EXPLORE_AUDIO_PUBLICATIONS } from "../graphql/explorePublications";
+import React from "react";
+
+export default function ExplorePublicationsTest() {
+  const { data, loading, error } = useQuery(EXPLORE_AUDIO_PUBLICATIONS, {
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  });
+
+  return data ? (
+    <div>
+      {data.explorePublications.items.map((publication) => (
+        <div>{publication.metadata.name}</div>
+      ))}
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
+}
