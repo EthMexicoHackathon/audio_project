4bb75613dd944870d119f64553210d3b2f525dc7
henrique <bongador@protonmail.com>
2022-08-20 22:03:39 -0500

    Footer change

--- website/src/components/Footer.js
+++ website/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Footer,Anchor,Image,Paragraph } from 'grommet';
 export default function FooterComponent (props) {
   return(
     <Footer background="brand" pad="medium" size="small">
-      <Anchor href="https://github.com/henrique1837/thevibes" target="_blank" rel="noreferrer">Github</Anchor>
+      <Anchor href="https://github.com/EthMexicoHackathon/audio_project" target="_blank" rel="noreferrer">Github</Anchor>
 
     </Footer>
   )
